77e55afa73e9fbb14cc811e57db9ae32a6011689
otto <otto@mgzl.jp>
2014-01-07 19:11:21 +0900

    完成？

--- wp_ajax_get_post.php
+++ wp_ajax_get_post.php
@@ -35,12 +35,12 @@
  * https://github.com/10up/grunt-wp-plugin
  */
 
-register_activation_hook( __FILE__, array( 'WP_Ajax_Get_Post', 'on_activation' ) );
-register_deactivation_hook( __FILE__, array( 'WP_Ajax_Get_Post', 'on_deactivation' ) );
-register_uninstall_hook( __FILE__, array( 'WP_Ajax_Get_Post', 'on_uninstall' ) );
+register_activation_hook( __FILE__, array( 'WP_Ajax_Get_Posts', 'on_activation' ) );
+register_deactivation_hook( __FILE__, array( 'WP_Ajax_Get_Posts', 'on_deactivation' ) );
+register_uninstall_hook( __FILE__, array( 'WP_Ajax_Get_Posts', 'on_uninstall' ) );
 
-add_action( 'plugins_loaded', array( 'WP_Ajax_Get_Post', 'init' ) );
-class WP_Ajax_Get_Post {
+add_action( 'plugins_loaded', array( 'WP_Ajax_Get_Posts', 'init' ) );
+class WP_Ajax_Get_Posts {
 	protected static $instance;
 	protected static $version = '0.1.0';
 	protected static $plugin_name = 'WP Ajax Get Post';
@@ -92,15 +92,23 @@ class WP_Ajax_Get_Post {
 	}
 
 	public function __construct() {
-		$this->plugin_url = plugin_dir_url( __FILE__ );
-		$this->plugin_path = dirname( __FILE__ ) . '/';
-
-		$locale = apply_filters( 'plugin_locale', get_locale(), 'wpagp' );
-		load_textdomain( 'wpagp', WP_LANG_DIR . '/wpagp/wpagp-' . $locale . '.mo' );
-		load_plugin_textdomain( 'wpagp', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-		// add action
+		add_action('wp_ajax_nopriv_'.$this->add_prefix('get_posts'), array($this,'ajax_get_posts') );
+		add_action('wp_ajax_'.$this->add_prefix('get_posts'), array($this,'ajax_get_posts') );
 	}
 
+	public function ajax_get_posts(){
+		header("Content-Type: application/javascript; charset=utf-8",true,200);
+		header("Access-Control-Allow-Origin: *");
+
+		$callback_name = $_REQUEST['callback'];
+		$callback_template = $callback_name.'(%s)';
+		$query = wp_parse_args(preg_replace('/^\?/', '', strstr($_SERVER["REQUEST_URI"], '?')));
+		$posts = get_posts($query);
+		$posts = $this->get_cf_data($posts);
+		apply_filters($this->add_prefix('ajax_get_posts'),$posts);
+		echo sprintf($callback_template,json_encode($posts));
+		die;
+	}
 
 	/**
 	 * get all custom field data;
@@ -112,78 +120,6 @@ class WP_Ajax_Get_Post {
 		}
 		$post -> cf = $cf_array;
 		return $post;
-	}
-
-	/*
-	 * 引数をもとにカスタム投稿タイプを生成します。
-	 * @param string $en 英語名（スラッグ）
-	 * @param string $ja 日本語名（表示用）
-	 * @param number $menu_position メニュー位置
-	 * @param array $taxonomies カスタムタクソノミーの設定
-	 */
-	public static function create_custom_post_type($en,$ja,$menu_position,$taxonomies = null,$show_in_menu = true){
-		if(!is_null($taxonomies) && is_array($taxonomies)){
-			$this->create_custom_taxonomies($taxonomies['en'],$taxonomies['ja'],$en);
-		}
-
-		register_post_type( $en,
-			array(
-			'labels' => array(
-			'name' => __( $ja ),
-			'singular_name' => __( $ja ),
-			'add_new_item' => __( $ja.'を新規追加' ),
-			'add_new' => __( $ja.'を新規追加' ),
-			'new_item' => __('新しい'.$ja),
-			'view_item' => __($ja.'を表示'),
-			'not_found' => __($ja.'はありません'),
-			'not_found_in_trash' => __('ゴミ箱に'.$ja.'はありません。'),
-			'search_items' => __($ja.'を検索'),
-			'description' => __($ja.'の管理をします。')
-			),
-			'has_archive' => true,
-			'public' => true,
-			'menu_position' => $menu_position,
-			'supports' => array('title','editor','author','custom-fields','revisions'),
-			'rewrite' => true,
-			'show_in_menu' => $show_in_menu
-			)
-		);
-	}
-
-	/**
-	 * カスタムタクソノミーを生成
-	 * @param string $en 英語名（スラッグ）
-	 * @param string $ja 日本語名（表示用）
-	 * @param number $post_type 追加するカスタム投稿スラッグ
-	 */
-	public static function create_custom_taxonomies($en,$ja,$post_type){
-		register_taxonomy(
-			$en, 
-			$post_type, 
-			array(
-				'hierarchical' => true, 
-				'update_count_callback' => '_update_post_term_count',
-				'label' => $ja,
-				'singular_label' => $ja,
-				'public' => true,
-				'show_ui' => true,
-				'rewrite' => true
-			)
-		);
-	}
-
-	/**
-	 * prefix付きでoptionを取得
-	 */
-	private static function get_option($key,$default = false){
-		return get_option($this->add_prefix($key),$default);
-	}
-
-	/**
-	 * prefix付きでoptionを保存
-	 */
-	private function update_option($key,$value){
-		return update_option($this->add_prefix($key),$value);
 	}
 
 	/**
